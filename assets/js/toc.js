a0aef7651957326181c7639011d53d2aa64b1ab4
materialknight <95397606+materialknight@users.noreply.github.com>
2025-12-22 00:13:37 -0600

    fix: Improved TOC functionality.

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -1,58 +1,87 @@
 "use strict"
 
 const toc = document.getElementById("toc")
-const headings = Array.from(document.querySelectorAll("h1[id], h2[id], h3[id], h4[id], h5[id], h6[id]"))
-const text_sections = Array.from(document.querySelectorAll("h1[id], h2[id], h3[id], h4[id], h5[id], h6[id], p[id]"))
+const text_sections = Array.from(document.querySelectorAll("h1, h2, h3, h4, h5, h6, p"))
+// const headings = text_sections.filter(txt_sec => /^h[1-6]$/i.test(txt_sec.tagName))
 
+// Map every <p>, <h1>, <h2>, etc. to a <li> in the table of contents.
 const zip = []
 let toc_heading = null
+let href = window.location.pathname.split("/").at(-1)
 for (const txt_section of text_sections)
 {
-   // txt_section is <h1>, <h2>... or <h6>
-   if (/h\d/i.test(txt_section.tagName))
+   if (/^h1$/i.test(txt_section.tagName)) // txt_section is <h1>
    {
-      toc_heading = toc.querySelector(`[href="#${txt_section.id}"]`).closest("li")
-      zip.push([txt_section, toc_heading])
+      toc_heading = toc.querySelector(`[href="${href}"]`)?.closest("li")
    }
-   else // txt_section is a <p>
+   else if (/^h[2-6]$/i.test(txt_section.tagName))// txt_section is <h2>, <h3>... or <h6>
+   {
+      const toc_li = toc.querySelector(`[href="${href}#${txt_section.id}"]`)?.closest("li")
+      if (toc_li)
+      {
+         toc_heading = toc_li
+      }
+   }
+   if (toc_heading)
    {
       zip.push([txt_section, toc_heading])
    }
 }
+
 const toc_map = new Map(zip)
+const headings = Array.from(toc_map.keys())
 let cleanup = []
 
-window.addEventListener("scrollend", () => {
+window.addEventListener("scrollend", mark_toc)
+toc.addEventListener("toggle", scroll_toc)
+
+mark_toc()
+
+function mark_toc() {
+   // De-highlight from the table of contents all previously highlighted <li>.
    cleanup.forEach(li => {
       li.classList.remove("visible_toc_entry")
    })
-
+   // Collect headings within viewport.
    const visible_headings = headings.filter(heading => {
       const rect = heading.getBoundingClientRect()
       return rect.top >= -5 && rect.bottom <= window.innerHeight
    })
 
+   // Highlight in the table of contents all visible headings.
+   console.log(visible_headings) // modify below:
    cleanup = visible_headings.map(heading => {
       const toc_li = toc_map.get(heading)
       toc_li.classList.add("visible_toc_entry")
       return toc_li
    })
-
+   // If any heading was highlighted, there's nothing else to do.
    if (visible_headings.length > 0) return
-
-   let txt_elem = text_sections.find(txt_sec => {
+   // Get the index of 1st element below the viewport's top, even if it goes past the viewport's bottom.
+   const txt_elem_i = text_sections.findIndex(txt_sec => {
       const rect = txt_sec.getBoundingClientRect()
-      return rect.top >= 0 && rect.bottom <= window.innerHeight
+      return rect.top >= 0
    })
+   // Get the last heading that is before the 1st visible element.
+   const current_heading = text_sections
+      .filter((txt_elem, i) => /^h[1-6]$/i.test(txt_elem.tagName) && parseInt(i) <= parseInt(txt_elem_i))
+      .at(-1)
 
-   while (txt_elem = txt_elem.previousElementSibling)
+   if (current_heading)
    {
-      if (/h\d/i.test(txt_elem.tagName))
-      {
-         const toc_li = toc_map.get(txt_elem)
-         toc_li.classList.add("visible_toc_entry")
-         cleanup.push(toc_li)
-         break
-      }
+      const toc_li = toc_map.get(current_heading)
+      toc_li.classList.add("visible_toc_entry")
+      cleanup.push(toc_li)
    }
-})
+   else
+   {
+      console.warn("There seems to be some <p> before the <h1>.")
+   }
+}
+
+function scroll_toc(toggle_ev) {
+   if (toggle_ev.newState === "open")
+   {
+      toc.querySelector(".visible_toc_entry").scrollIntoView({ block: "center" })
+   }
+}
